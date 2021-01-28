5baea943a3201d28b02450e7e8d9ba459ba5c4a8
Kordzakhiaa <giorgi.kordzakhia.2@btu.edu.ge>
2021-01-28 22:25:43 +0400

    Added db.models Car,Order,Customer,CarWasher

--- car_wash/admin.py
+++ car_wash/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(CarWasher)
+admin.site.register(Order)
+admin.site.register(Car)
+admin.site.register(Customer)
--- car_wash/models.py
+++ car_wash/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class CarWasher(models.Model):
+    name = models.CharField(max_length=200,)
+
+    def __str__(self):
+        return self.name
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    # car = models.OneToOneField(Car, max_length=200, on_delete=models.CASCADE)
+    # order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Car(models.Model):
+    TYPES = (
+        ('crossover', 'crossover'),
+        ('sedan', 'sedan'),
+        ('hatchback', 'hatchback')
+    )
+
+    car_type = models.CharField(max_length=200, null=False, choices=TYPES)
+    license_plate = models.CharField(max_length=30, null=False)
+    car_owner = models.OneToOneField(
+        Customer, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.car_type}, {self.license_plate}"
+
+
+class Order(models.Model):
+    order_start_date = models.DateTimeField(verbose_name="Order Start Date")
+    order_end_date = models.DateTimeField(verbose_name="Order End Date")
+    washer = models.ForeignKey(CarWasher, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
