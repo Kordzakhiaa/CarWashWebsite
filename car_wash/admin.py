48d9c63838ecf4385691ba551a47ab2a616b8e01
Kordzakhiaa <giorgi.kordzakhia.2@btu.edu.ge>
2021-02-02 23:19:51 +0400

    Modified db.models and added some orders for testing

--- car_wash/admin.py
+++ car_wash/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import *
+from .models import Order, WashType, CarType, CarWasher, Car, Customer
 
-admin.site.register(CarWasher)
 admin.site.register(Order)
+admin.site.register(WashType)
+admin.site.register(CarType)
+admin.site.register(CarWasher)
 admin.site.register(Car)
 admin.site.register(Customer)
--- car_wash/models.py
+++ car_wash/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
 class CarWasher(models.Model):
     name = models.CharField(max_length=200)
-    # salary = 
+
+    # salary = models.DecimalField(max_digits=4, decimal_places=4)
 
     def __str__(self):
         return self.name
@@ -16,37 +18,45 @@ class Customer(models.Model):
         return self.name
 
 
-class Car(models.Model):
-    TYPES = (
-        ('crossover', 'crossover'),
-        ('sedan', 'sedan'),
-        ('hatchback', 'hatchback')
-    )
-
-    # PRICES = (
-    #     ('crossover', 20),
-    #     ('sedan', 15),
-    #     ('hatchback', 10)
-    # )
-
-    car_type = models.CharField(max_length=200, null=False, choices=TYPES)
-    license_plate = models.CharField(max_length=30, null=False)
-    car_owner = models.OneToOneField(
-        Customer, on_delete=models.CASCADE, null=True, blank=True)
+class WashType(models.Model):
+    name = models.CharField(max_length=50, verbose_name=_("Name of wash type"))
+    percentage = models.IntegerField(verbose_name=_("Base price percentage"), default=100)
 
     def __str__(self):
-        return f"{self.car_type}, {self.license_plate}"
+        return self.name
+
+
+class CarType(models.Model):
+    name = models.CharField(max_length=50, verbose_name=_('Car-Type'), unique=True)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class Car(models.Model):
+    car_type = models.ForeignKey(CarType, on_delete=models.SET_NULL, null=True)
+    license_plate = models.CharField(max_length=30, null=False)
+
+    def __str__(self):
+        return f"{self.license_plate}>"
 
 
 class Order(models.Model):
-    STATUS = (
-        ('Pending', 'Pending'),
-        ('Finished', 'Finished'),
-    )
+    car = models.ForeignKey(Car, on_delete=models.PROTECT, related_name='orders', default=None)
+    washer = models.ForeignKey(CarWasher, on_delete=models.CASCADE, related_name='orders')
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, related_name='orders')
+    wash_type = models.ForeignKey(WashType, on_delete=models.PROTECT, related_name='orders', default=None)
+    order_price = models.DecimalField(max_digits=4, decimal_places=2, verbose_name=_("Price"), default=None)
+    created_date = models.DateTimeField(auto_now=True, verbose_name=_("Created Date"))
+    order_start_date = models.DateTimeField(verbose_name=_("Schedule start time"))
+    order_end_date = models.DateTimeField(verbose_name=_("Schedule end time"))
+    status = models.CharField(max_length=200, default=None)
 
-    washer = models.ForeignKey(CarWasher, on_delete=models.CASCADE)
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    order_start_date = models.DateTimeField(verbose_name="Order Start Date")
-    order_end_date = models.DateTimeField(verbose_name="Order End Date")
-    status = models.CharField(max_length=200, null=True, choices=STATUS)
-    # car_details = models.OneToOneField(Car, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.customer}'s car {self.car} using {str(self.wash_type).lower()} washing package"
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.order_price = self.car.car_type.price * self.wash_type.percentage / 100
+        super(Order, self).save(*args, **kwargs)
