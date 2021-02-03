6a797fb75a134094d64ecda243c0a815dbcba5fb
Kordzakhiaa <giorgi.kordzakhia.2@btu.edu.ge>
2021-02-03 22:13:12 +0400

    modified views added weekly, monthly, and yearly quantity of washed cars, also modified html files

--- car_wash/views.py
+++ car_wash/views.py
@@ -1,5 +1,7 @@
+from datetime import datetime, timedelta
 from django.shortcuts import render
 from .models import *
+from car_wash.get_date import date
 
 
 def home(request):
@@ -16,9 +18,14 @@ def washer_detail(request, pk: int):
     earned_money = 0
     for i in filter_by_washer_name:
         earned_money += i.order_price
+    orders = washer_by_id.orders.all()
+    today = datetime.today()
     context = {
         'washer_by_id': washer_by_id,
-        'earned_money': earned_money
+        'earned_money': earned_money,
+        "weekly": orders.filter(created_date__range=[date(7), today]).count(),
+        "monthly": orders.filter(created_date__range=[date(30), today]).count(),
+        "yearly": orders.filter(created_date__range=[date(365), today]).count()
     }
     return render(request, 'washer_details.html', context)
 
