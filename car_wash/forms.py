54f81f3f6c3d0f604d5fb6f7e6d9e7f728c9a974
Kordzakhiaa <giorgi.kordzakhia.2@btu.edu.ge>
2021-02-12 21:56:10 +0400

    modified forms.py  added widgets for datetimefield

--- car_wash/forms.py
+++ car_wash/forms.py
@@ -1,8 +1,13 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, forms, DateTimeField
 from .models import Order, Car
+from django import forms
+from django.forms import EmailField, CharField, Textarea, ModelChoiceField, TextInput
 
 
 class OrderForm(ModelForm):
+    order_start_date = forms.DateTimeField(widget=forms.TextInput(attrs={'type': 'datetime-local'}))
+    order_end_date = forms.DateTimeField(widget=forms.TextInput(attrs={'type': 'datetime-local'}))
+
     class Meta:
         model = Order
         fields = '__all__'
